5eb922b4ecb2d933eb574b38dde1b9de94bf8144
Sam Day <me@samcday.com>
2023-05-11 09:42:42 +0200

    initial commit - basic node metadata reconciliation

---
+++ src/main.rs
@@ -0,0 +1,225 @@
+use std::collections::BTreeMap;
+use std::sync::Arc;
+use std::time::Duration;
+use futures::StreamExt;
+use k8s_openapi::api::batch::v1::Job;
+use k8s_openapi::api::core::v1::{Node, NodeAddress, Pod};
+use k8s_openapi::serde::Deserialize;
+use kube::{Client};
+use kube::api::{DeleteParams, ListParams, LogParams, Patch, PatchParams, PostParams};
+use kube::runtime::{conditions, Controller, watcher};
+use kube::runtime::controller::Action;
+use kube::runtime::wait::await_condition;
+use tracing::{info, warn};
+use thiserror::Error;
+use tokio::time::error::Elapsed;
+
+const LABEL_ZONE: &str = "topology.kubernetes.io/zone";
+const LABEL_REGION: &str = "topology.kubernetes.io/region";
+const TAINT_UNINITIALIZED: &'static str = "node.cloudprovider.kubernetes.io/uninitialized";
+
+struct Data {
+    client: Client
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Node missing .spec")]
+    NodeMissingSpec,
+    #[error("k8s error: {0}")]
+    KubeError(#[source] kube::Error),
+    #[error("json error: {0}")]
+    SerdeJsonError(#[source] serde_json::Error),
+    #[error("yaml error: {0}")]
+    SerdeYamlError(#[source] serde_yaml::Error),
+    #[error("timout waiting for job to complete: {0}")]
+    JobTimeout(#[source] Elapsed),
+    #[error("couldn't find completed Pod for Job")]
+    JobMissingPod,
+    #[error("hcloud metadata missing: {0}")]
+    MetadataMissing(String)
+}
+
+impl From<kube::Error> for Error {
+    fn from(v: kube::Error) -> Self {
+        Error::KubeError(v)
+    }
+}
+
+impl From<serde_json::Error> for Error {
+    fn from(v: serde_json::Error) -> Self {
+        Error::SerdeJsonError(v)
+    }
+}
+
+impl From<serde_yaml::Error> for Error {
+    fn from(v: serde_yaml::Error) -> Self {
+        Error::SerdeYamlError(v)
+    }
+}
+
+impl From<Elapsed> for Error {
+    fn from(v: Elapsed) -> Self {
+        Error::JobTimeout(v)
+    }
+}
+
+async fn reconcile_node_adoption(node: Arc<Node>, ctx: Arc<Data>) -> Result<Action, Error> {
+    let client = &ctx.client;
+    let deleted = node.metadata.deletion_timestamp.is_some();
+    let provider_id = &node.spec.as_ref().ok_or_else(|| Error::NodeMissingSpec)?.provider_id;
+
+    let node_name = node.metadata.name.as_ref().unwrap();
+    let node_uid = node.metadata.uid.as_ref().unwrap();
+
+    let job_name = format!("hcc-metadata-{}", node_uid);
+
+    let jobs = kube::Api::<Job>::default_namespaced(client.clone());
+    let pods = kube::Api::<Pod>::default_namespaced(client.clone());
+    let nodes = kube::Api::<Node>::all(client.clone());
+
+    let job = jobs.get_opt(&job_name).await?;
+
+    let taints = node.spec.as_ref().unwrap().clone().taints.unwrap_or(vec![]);
+    let tainted = taints.iter().any(|x| x.key.eq(TAINT_UNINITIALIZED));
+
+    if deleted || provider_id.is_some() || !tainted {
+        if let Some(job) = job {
+            info!("deleting job {:?}", job.metadata.name);
+            jobs.delete(&job_name, &DeleteParams::background()).await?;
+        }
+        return Ok(Action::await_change());
+    }
+
+    if job.is_none() {
+        let data = serde_json::from_value(serde_json::json!({
+            "apiVersion": "batch/v1",
+            "kind": "Job",
+            "metadata": {
+                "name": &job_name,
+            },
+            "spec": {
+                "template": {
+                    "spec": {
+                        "containers": [{
+                            "name": "metadata",
+                            "image": "curlimages/curl:latest",
+                            "args": [
+                                "-s",
+                                "--fail",
+                                "--retry",
+                                "3",
+                                "http://169.254.169.254/hetzner/v1/metadata",
+                            ],
+                        }],
+                        "hostNetwork": true,
+                        "nodeName": node_name,
+                        "restartPolicy": "Never",
+                        "tolerations": [{
+                            "operator": "Exists",
+                        }],
+                    },
+                },
+                "ttlSecondsAfterFinished": 600,
+            },
+        }))?;
+        jobs.create(&PostParams::default(), &data).await?;
+    }
+
+    let cond = await_condition(jobs.clone(), &job_name, conditions::is_job_completed());
+    let _ = tokio::time::timeout(Duration::from_secs(15), cond).await?;
+
+    let job_pods = pods.list(&ListParams::default().labels(&("job-name=".to_string()+&job_name))).await?;
+    let mut job_pod = None;
+    for pod in job_pods {
+        if let Some(status) = pod.status {
+            if let Some(phase) = status.phase {
+                if phase == "Succeeded" {
+                    job_pod = Some(pod.metadata.name.unwrap());
+                }
+            }
+        }
+    }
+
+    let job_pod = job_pod.ok_or_else(|| Error::JobMissingPod)?;
+    let output = pods.logs(&job_pod, &LogParams::default()).await?;
+    let metadata = serde_yaml::Value::deserialize(serde_yaml::Deserializer::from_str(&output))?;
+
+    let node_status = node.status.as_ref().unwrap();
+
+    if let Some(serde_yaml::Value::String(public_ipv4)) = metadata.get("public-ipv4") {
+        let mut addresses = node_status.addresses.clone().unwrap_or(vec![]);
+        if !addresses.iter().any(|x| x.type_.eq("ExternalIP")) {
+            info!("patching Node {} with external IP {}", node_name, public_ipv4);
+            addresses.push(NodeAddress{
+                type_: "ExternalIP".to_string(),
+                address: public_ipv4.clone(),
+            });
+            nodes.patch_status(node_name, &PatchParams::default(), &Patch::Merge(serde_json::json!({
+                "status": {
+                    "addresses": addresses,
+                }
+            }))).await?;
+        }
+    }
+
+    if let Some(serde_yaml::Value::String(az)) = metadata.get("availability-zone") {
+        if let Some(zone) = az.split("-").next() {
+            if let Some(serde_yaml::Value::String(region)) = metadata.get("region") {
+                let labels = node.metadata.labels.clone().unwrap_or(BTreeMap::default());
+                if !labels.contains_key(LABEL_ZONE) && !labels.contains_key(LABEL_REGION) {
+                    info!("patching Node {} with topology {} {}", node_name, region, zone);
+                    nodes.patch_metadata(node_name, &PatchParams::default(), &Patch::Merge(serde_json::json!({
+                        "metadata": {
+                            "labels": {
+                                LABEL_REGION: format!("hcloud-{}", region),
+                                LABEL_ZONE: format!("hcloud-{}", zone),
+                            },
+                        },
+                    }))).await?;
+                }
+            }
+        }
+    }
+
+    let id = metadata.get("instance-id").ok_or_else(|| Error::MetadataMissing("instance-id".to_string()))?.as_u64().unwrap();
+
+    info!("patching Node {} with provider ID and removing taint", node_name);
+    nodes.patch(node_name, &PatchParams::default(), &Patch::Merge(serde_json::json!({
+        "spec": {
+            "providerID": format!("hcloud://{}", id),
+            "taints": taints.iter().filter(|x| x.key != TAINT_UNINITIALIZED).collect::<Vec<_>>(),
+        },
+    }))).await?;
+
+    //  * set internal IPs if any private-networks
+    //  * set providerID to instanceID
+    Ok(Action::requeue(Duration::from_secs(60)))
+}
+
+fn error_policy(_object: Arc<Node>, _error: &Error, _ctx: Arc<Data>) -> Action {
+    Action::requeue(Duration::from_secs(1))
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let client = Client::try_default().await?;
+
+    let nodes = kube::Api::<Node>::all(client.clone());
+    let jobs = kube::Api::<Job>::all(client.clone());
+
+    Controller::new(nodes, watcher::Config::default())
+        .owns(jobs, watcher::Config::default())
+        .shutdown_on_signal()
+        .run(reconcile_node_adoption, error_policy, Arc::new(Data { client }))
+        .for_each(|res| async move {
+            match res {
+                Ok(o) => info!("reconciled {:?}", o),
+                Err(e) => warn!("reconciliation error: {:?}", e),
+            }
+        })
+        .await;
+
+    Ok(())
+}
